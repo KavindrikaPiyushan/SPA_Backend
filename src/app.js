1885d13f3e00635d86b7f125502448c19478c67c
KavindrikaPiyushan <kavindrikapiyushan@gmail.com>
2025-12-22 17:58:58 +0530

    fixed cors  issue

--- src/app.js
+++ src/app.js
@@ -9,7 +9,10 @@ import adminRoutes from './routes/admin.routes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(cookieParser());
 
 app.use(express.json());
