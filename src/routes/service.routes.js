5c2f0a8c25a9c3500eb957c42048737bf3848d1c
KavindrikaPiyushan <kavindrikapiyushan@gmail.com>
2026-01-11 18:40:02 +0530

    get incative services api completed

--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createService,getServices, getServiceById,updateService,deleteService} from '../controller/service.controller.js';
+import {createService,getServices, getServiceById,updateService,deleteService,getInactiveServices} from '../controller/service.controller.js';
 import { authenticate } from '../utils/auth.js';
 const router = express.Router();
 
 router.post('/createService', createService);
 router.get('/getServices', getServices);
+router.get('/getInactiveServices', getInactiveServices);
 router.get('/getService/:sid', getServiceById);
 router.put('/updateService/:sid', updateService);
 router.delete('/deleteService/:sid', deleteService);
